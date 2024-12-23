c779e015bce0bc3249c8646a8239c47aa35daf1c
waleedalikhan <waleedomore@gmail.com>
2024-12-23 17:12:39 +0500

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,7 @@
+import Alpine from "alpinejs";
+import variantOne from "./variant-one";
+
+window.Alpine = Alpine;
+
+// Initializing AlpineJS
+Alpine.start();
---
+++ src/variant-one.ts
@@ -0,0 +1,3 @@
+export default () => ({
+  drawerHeading: "Drawer heading",
+});
